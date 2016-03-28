a1c73b65f0e5c3d821910b1da1e0ccfc990e0038
TroyHernandez <troy.a.hernandez@gmail.com>
2016-03-28 02:27:12 -0500

    Added d/r lines to plots and made predictions based on data.

--- Target538.R
+++ Target538.R
@@ -29,6 +29,22 @@ text(x = past.dat$SANDERS.Target[big.sanders] - c(10, rep(6, 5)),
      y = past.dat$SANDERS.Won[big.sanders],
      labels = past.dat$STATE.OR.TERRITORY[big.sanders], cex = .5)
 abline(0, 1)
+#####################################################
+# Modeling performance by partisanship
+color.str2 <- replace(color.str, which(color.str == "red"), "red.purp")
+color.str2 <- replace(color.str2, which(color.str2 == "purple"), "red.purp")
+
+mod.sanders.blue <- lm(past.dat$SANDERS.Won[color.str2 == "blue"] ~ 0 +
+                         past.dat$SANDERS.Target[color.str2 == "blue"])
+# mod$coefficients
+# Blue line is shows Bernie's average over-performance in blue states 
+abline(0, mod.sanders.blue$coefficients[1], col = "blue")
+
+mod.sanders.red <- lm(past.dat$SANDERS.Won[color.str2 == "red.purp"] ~ 0 +
+                        past.dat$SANDERS.Target[color.str2 == "red.purp"])
+# mod$coefficients
+# Red line is shows Bernie's average under-performance in red/purple states 
+abline(0, mod.sanders.red$coefficients[1], col = "red")
 
 #####################################################
 Clinton <- past.dat$CLINTON.Won - past.dat$CLINTON.Target
@@ -42,15 +58,60 @@ plot(x = past.dat$CLINTON.Target, y = past.dat$CLINTON.Won,
 
 big.clinton <- which(past.dat$CLINTON.Target > 50)
 
-text(x = past.dat$CLINTON.Target[big.clinton] - rep(6, length(big.clinton)),
+text(x = past.dat$CLINTON.Target[big.clinton] - rep(8, length(big.clinton)),
      y = past.dat$CLINTON.Won[big.clinton],
      labels = past.dat$STATE.OR.TERRITORY[big.clinton], cex = .5)
 abline(0, 1)
 
 #####################################################
 
+mod.clinton.blue <- lm(past.dat$CLINTON.Won[color.str2 == "blue"] ~ 0 +
+                         past.dat$CLINTON.Target[color.str2 == "blue"])
+# mod$coefficients
+# Blue line is shows Hilary's average under-performance in blue states 
+abline(0, mod.clinton.blue$coefficients[1], col = "blue")
+
+mod.clinton.red <- lm(past.dat$CLINTON.Won[color.str2 == "red.purp"] ~ 0 +
+                        past.dat$CLINTON.Target[color.str2 == "red.purp"])
+# mod$coefficients
+# Red line is shows Hillary's average over-performance in red/purple states 
+abline(0, mod$coefficients[1], col = "red")
+
+#####################################################
+# Percent of delegates pledged:
+sum(past.dat$TOTAL.DELEGATES) / sum(dat$TOTAL.DELEGATES)
+
+# Percent of past delegates from red or purple states
+sum(past.dat$TOTAL.DELEGATES[past.dat$COLOR == "red" |
+                              past.dat$COLOR == "purple"]) /
+  sum(past.dat$TOTAL.DELEGATES)
+
+# Percent of past delegates from blue states
+sum(past.dat$TOTAL.DELEGATES[past.dat$COLOR == "blue"]) /
+  sum(past.dat$TOTAL.DELEGATES)
+
+# Percent of future delegates from red or purple states
 future.dat <- dat[future.ind, ]
-sum(future.dat$CLINTON.Target)
-sum(past.dat$CLINTON.Target)
-sum(future.dat$SANDERS.Target)
-sum(past.dat$SANDERS.Target)
+sum(future.dat$TOTAL.DELEGATES[future.dat$COLOR == "red" |
+                                future.dat$COLOR == "purple"]) /
+  sum(future.dat$TOTAL.DELEGATES)
+
+# Percent of future delegates from blue states
+sum(future.dat$TOTAL.DELEGATES[future.dat$COLOR == "blue"]) /
+  sum(future.dat$TOTAL.DELEGATES)
+##########################################################
+# Bernie still comes up short
+sum(future.dat$SANDERS.Target[future.dat$COLOR == "blue"] *
+    mod.sanders.blue$coefficients) + # 1.25) +#
+  sum(future.dat$SANDERS.Target[future.dat$COLOR == "red" |
+                                  future.dat$COLOR == "purple"] *
+        mod.sanders.red$coefficients) + 1037
+# [1] 1945.444
+
+# Hillary eeks it out
+sum(future.dat$CLINTON.Target[future.dat$COLOR == "blue"] *
+      mod.clinton.blue$coefficients) +
+  sum(future.dat$CLINTON.Target[future.dat$COLOR == "red" |
+                                  future.dat$COLOR == "purple"] *
+        mod.clinton.red$coefficients) + 1267
+# [1] 2102.357
